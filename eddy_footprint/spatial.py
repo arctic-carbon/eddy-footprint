abf551b8f537b4f07a9e26ecfa88aa48b47a0f51
Ludwig <Ludwig.Ludda@epa.gov>
2025-04-10 12:29:10 -0400

    change length not counts

--- eddy_footprint/spatial.py
+++ eddy_footprint/spatial.py
@@ -14,9 +14,9 @@ def rotate_domain(da, *, wind_direction):
 
 
 def build_domain(*, domain_length: int, resolution: int, time: np.ndarray):
-    x = np.linspace(0, domain_length, int(domain_length / resolution+1))
+    x = np.linspace(0, domain_length-resolution, int(domain_length / resolution))
     y = np.linspace(
-        -domain_length / 2, domain_length / 2, int(domain_length / resolution+1)
+        -(domain_length-resolution) / 2, (domain_length -resolution)/ 2, int(domain_length / resolution)
     )
     xx, yy = np.meshgrid(x, y)
     data = np.zeros(
@@ -61,10 +61,10 @@ def normalize_domain(
 
 def build_template(*, domain_length, resolution):
     template_x = np.linspace(
-        -domain_length, domain_length, (int(domain_length / resolution) * 2+1)
+        -domain_length, domain_length-resolution, (int(domain_length / resolution) * 2)
     )
     template_y = np.linspace(
-        -domain_length, domain_length, (int(domain_length / resolution) * 2+1)
+        -domain_length, domain_length-resolution, (int(domain_length / resolution) * 2)
     )
     template_xx, template_yy = np.meshgrid(template_x, template_y, indexing="xy")
     query_points = np.array((template_xx.flatten(), template_yy.flatten())).transpose()
